516c4a0f6b5bfd7296fa3fea20182cbc4e5e98b4
nh0126 <nh0126@sookmyung.ac.kr>
2023-09-21 16:03:49 +0900

    Create practice03.c

---
+++ practice03.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	char input_char, output_char;
+	
+	printf("enter a character : ");
+	scanf("%c", &input_char);
+	
+	output_char = input_char + 1;
+	
+	printf("the next character of %c (%d) is %c (%d)\n", input_char, input_char, output_char, output_char);
+	
+	return 0;
+}
